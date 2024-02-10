c616add4eceb800c6512a7867845c6e1adbeff46
Philippe Matray <phmatray@gmail.com>
2024-02-10 13:44:05 +0100

    set GPL-3.0-or-later in stylecop.json licenseName and update code headers

--- src/library/MasterCommander/Core/Display/IConsole.cs
+++ src/library/MasterCommander/Core/Display/IConsole.cs
@@ -1,5 +1,5 @@
 // Copyright (c) 2020-2024 Atypical Consulting SRL. All rights reserved.
-// Atypical Consulting SRL licenses this file to you under the MIT license.
+// Atypical Consulting SRL licenses this file to you under the GPL-3.0-or-later license.
 // See the LICENSE file in the project root for full license information.
 
 namespace MasterCommander.Core.Display;
